72368ba18dd610fb591bf2c99fd62dd1641c4ad5
Annedrew <1196244890@qq.com>
2024-04-06 15:23:57 +0200

    Add project section.

---
+++ components/projects.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SectionHeader from "./section-header";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeader>My Projects</SectionHeader>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
--- components/section-header.tsx
+++ components/section-header.tsx
@@ -6,5 +6,5 @@ type SectionHeaderProps = {
 }
 
 export default function SectionHeader({ children }: SectionHeaderProps) {
-  return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+  return <h2 className="text-3xl font-medium capitalize mb-8 text-center">{children}</h2>;
 }
