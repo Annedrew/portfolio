3df07a9f7eccafbff9e11d29f5afb78071857b99
Annedrew <1196244890@qq.com>
2024-04-18 10:47:06 +0200

    Bugfix: link certification to certification.

--- components/certification/certification.tsx
+++ components/certification/certification.tsx
@@ -15,7 +15,7 @@ export default function Certification() {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      id="about"
+      id="certification"
     >
       <SectionHeader>Certification</SectionHeader>
       <div className={styles.cert}>
