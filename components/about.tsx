cf81be1ff1337815b20581bdebc910f5620f99d6
Annedrew <1196244890@qq.com>
2024-04-30 07:15:24 +0200

    Change apostrophe to HTML entity encodings: &apos;

--- components/about.tsx
+++ components/about.tsx
@@ -18,8 +18,8 @@ export default function About() {
         <SectionHeader>About Me</SectionHeader>
         <p className=''>
           I have been working with <b>full-stack development</b>, specializing in React, Next.js, Node.js, Express.js and Python recently.
-          I graduated from <b>Aalborg University</b>, with a master's degree in computer science.
-          I'm characterized by a <b>long-term</b> mindset, continuously pursuing a better way to build applications.
+          I graduated from <b>Aalborg University</b>, with a master&apos;s degree in computer science.
+          I&apos;m characterized by a <b>long-term</b> mindset, continuously pursuing a better way to build applications.
           My enthusiasm lies in building <b>high-quality</b> software and websites.
           Around 2 years of experience as a <b>System Test Engineer</b>, honed my skills including quality awareness, communication, and an organized approach to work.
           Passionate about staying updated with the latest technologies, I gained some certification on <b>Kubernetes</b> and <b>AWS</b> by self-learning. 
